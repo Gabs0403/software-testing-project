5e8173b79717d05d3092eb925d3273383f2c0836
cssmith0053 <151086974+cssmith0053@users.noreply.github.com>
2025-04-17 04:00:28 -0400

    Add files via upload

--- Membership.java
+++ Membership.java
@@ -9,11 +9,11 @@ import org.testng.annotations.*;
 import java.time.Duration;
 
 public class Membership {
-    // before every test load the page and instantiate objects of all the necessary tools
     WebDriver driver;
     WebDriverWait wait;
     Actions actions;
 
+    // load page and setup necessary tools before every method
     @BeforeMethod
     void loadPage() {
         driver = new ChromeDriver();
@@ -24,98 +24,92 @@ public class Membership {
         actions = new Actions(driver);
     }
 
-    // after every test close the page
+    // close page after every method
     @AfterMethod
     void closePage() {
         driver.quit();
     }
 
-    // fetch the current URL and page title
+    // get current URL and title of the page
     @Test(priority = 1)
-    void getCurrentUrl() {
-        // fetch the current URL
+    void urlAndTitle() {
         System.out.println("Current URL: " + driver.getCurrentUrl());
 
-        // fetch the page title
         System.out.println("Page Title: " + driver.getTitle());
     }
 
-    // scroll through the top and bottom carousels
     @Test(priority = 2)
     void carousels() throws InterruptedException {
         // wait for page to load
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"799c975e-c181-4b6b-b0ea-769e17198073\"]/div/section/div/div[2]/button[2]")));
 
+        // scroll down
         JavascriptExecutor exe = (JavascriptExecutor) driver;
         exe.executeScript("window.scrollBy(0, 1000);");
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
+        // iterate through the top carousel
         WebElement carouselBtnNext1 = driver.findElement(By.xpath("//*[@id=\"799c975e-c181-4b6b-b0ea-769e17198073\"]/div/section/div/div[2]/button[2]"));
-
         carouselBtnNext1.click();
-        Thread.sleep(500);
-
+        Thread.sleep(1000);
         carouselBtnNext1.click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
+        // iterate back through the top carousel
         WebElement carouselBtnPrev1 = driver.findElement(By.xpath("//*[@id=\"799c975e-c181-4b6b-b0ea-769e17198073\"]/div/section/div/div[2]/button[1]"));
-
+        carouselBtnPrev1.click();
+        Thread.sleep(500);
         carouselBtnPrev1.click();
         Thread.sleep(500);
 
-        carouselBtnPrev1.click();
-        Thread.sleep(500);
-
+        // scroll further down
         exe.executeScript("window.scrollBy(0, 1000);");
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
+        // iterate through the bottom carousel
         WebElement carouselBtnNext2 = driver.findElement(By.xpath("//*[@id=\"1c647239-6d06-40e2-8f00-baeb97eccb09\"]/div/section/div/div[2]/button[2]"));
-
         carouselBtnNext2.click();
-        Thread.sleep(500);
-
+        Thread.sleep(1000);
         carouselBtnNext2.click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
+        // iterate back through the bottom carousel
         WebElement carouselBtnPrev2 = driver.findElement(By.xpath("//*[@id=\"1c647239-6d06-40e2-8f00-baeb97eccb09\"]/div/section/div/div[2]/button[1]"));
-
         carouselBtnPrev2.click();
         Thread.sleep(500);
-
         carouselBtnPrev2.click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
     }
 
-    // scroll through the top and bottom carousels
     @Test(priority = 3)
     void faq() throws InterruptedException {
         // wait for page to load
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"80f81ad0-339d-49ec-a096-2e6425918eea__286dbec1-feda-4f8b-b134-8ae948bcfd5e__panel\"]/summary/span/span[2]")));
 
+        // open up all the sections of the FAQ
         WebElement faqDropDown1 = driver.findElement(By.xpath("//*[@id=\"80f81ad0-339d-49ec-a096-2e6425918eea__286dbec1-feda-4f8b-b134-8ae948bcfd5e__panel\"]/summary/span/span[2]"));
-
         faqDropDown1.click();
         Thread.sleep(1000);
 
         WebElement faqDropDown2 = driver.findElement(By.xpath("//*[@id=\"80f81ad0-339d-49ec-a096-2e6425918eea__77c46060-33fb-4681-bd86-f4df71e8e55f__panel\"]/summary/span/span[2]"));
-
         faqDropDown2.click();
         Thread.sleep(1000);
 
         WebElement faqDropDown3 = driver.findElement(By.xpath("//*[@id=\"80f81ad0-339d-49ec-a096-2e6425918eea__4c93e329-8b23-47e4-b298-7202bbe086fd__panel\"]/summary/span/span[2]"));
-
         faqDropDown3.click();
         Thread.sleep(1000);
 
+        // scroll down
         JavascriptExecutor exe = (JavascriptExecutor) driver;
         exe.executeScript("window.scrollBy(0, 250);");
         Thread.sleep(500);
 
+        // close all the sections of the FAQ
         faqDropDown3.click();
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         faqDropDown2.click();
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         faqDropDown1.click();
         Thread.sleep(1000);
@@ -129,10 +123,12 @@ public class Membership {
         wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
         WebElement email = driver.findElement(By.id("username"));
 
+        // test wrong email
         email.sendKeys("fgcucen4072testemail");
         email.sendKeys(Keys.ENTER);
         Thread.sleep(1000);
 
+        // test correct email
         email.sendKeys("@gmail.com");
         Thread.sleep(1000);
         email.sendKeys(Keys.ENTER);
@@ -148,10 +144,12 @@ public class Membership {
         wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
         WebElement email = driver.findElement(By.id("username"));
 
+        // test wrong email
         email.sendKeys("cs48898");
         email.sendKeys(Keys.ENTER);
         Thread.sleep(1000);
 
+        // test correct email
         email.sendKeys("@gmail.com");
         Thread.sleep(1000);
         email.sendKeys(Keys.ENTER);
